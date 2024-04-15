ab58002135b7bff8637b57fd9fb290ef3294256b
anaclarad <anaclaradjo813@gmail.com>
2024-04-15 18:30:47 -0300

    alterar

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ enchant();
 window.onload = function () {
   game = new Game(320, 320);
   game.fps = 24;
-  game.preload(["chara1.gif", "icon0.gif", "bg.png", "fire.png", "win.png"]);
+  game.preload(["chara1.gif", "icon0.gif", "bg.png", "win.png"]);
 
   game.onload = function () {
     bear = new Sprite(32, 32);
@@ -19,13 +19,12 @@ window.onload = function () {
     background.x = background.y = 0;
     background.image = game.assets["bg.png"];
 
-    
     var scoreLabel = new Label();
-    scoreLabel.x = 10; // Posição x do contador na tela
-    scoreLabel.y = 5; // Posição y do contador na tela
-    scoreLabel.color = "black"; // Cor do texto
-    scoreLabel.font = '16px "Arial"'; // Tamanho e tipo da fonte
-    scoreLabel.text = "0"; // Texto inicial do contador
+    scoreLabel.x = 10;
+    scoreLabel.y = 5;
+    scoreLabel.color = "black";
+    scoreLabel.font = '16px "Arial"';
+    scoreLabel.text = "0";
 
     game.rootScene.addEventListener("touchstart", function (e) {
       bear.x = e.localX;
@@ -36,28 +35,38 @@ window.onload = function () {
     });
 
     game.score = 0;
-
     game.rootScene.addEventListener("enterframe", function () {
-      if (game.frame % 6 == 0) {
-        addBanana();
+      if (game.frame % 10 == 0) {
+        if (game.score >= 0 && game.frame % 5 == 0) {
+          addBanana();
+        }
+        if (game.score >= 3 && game.frame % 5 == 0) {
+          addBomba();
+        }
+        if (game.score >= 15 && game.frame % 15 == 0) {
+          addDinamite();
+        }
+        if (game.score > 20 && game.frame % 12 == 0) {
+          addMorte();
+        }
+        if (game.score > 30 && game.frame % 18 == 0) {
+          addEstrela();
+        }
       }
-
       if (game.score >= 100) {
         victory();
-      }
-      if (game.score < 0) {
+      } else if (game.score < 0) {
         game.end(game.score, game.score + " bananas capturadas!");
       }
     });
 
-   
     game.rootScene.addEventListener("enterframe", function () {
       scoreLabel.text = "Score: " + game.score;
     });
 
     game.rootScene.addChild(background);
     game.rootScene.addChild(bear);
-    game.rootScene.addChild(scoreLabel); // Adiciona o contador à cena
+    game.rootScene.addChild(scoreLabel);
   };
   game.start();
 };
@@ -74,10 +83,50 @@ function addBanana() {
       game.rootScene.removeChild(this);
       game.score++;
     } else {
-      this.y += 3;
+      this.y += 6;
     }
   });
 
+  game.rootScene.addChild(banana);
+}
+
+function addEstrela() {
+  var estrela = new Sprite(16, 16);
+  estrela.x = rand(320);
+  estrela.y = 0;
+  estrela.image = game.assets["icon0.gif"];
+  estrela.frame = 30;
+
+  estrela.addEventListener("enterframe", function (e) {
+    if (this.intersect(bear)) {
+      game.rootScene.removeChild(this);
+      game.score += 3;
+    } else {
+      this.y += 5;
+    }
+  });
+  game.rootScene.addChild(estrela);
+}
+
+function addMorte() {
+  var morte = new Sprite(16, 16);
+  morte.x = rand(320);
+  morte.y = 0;
+  morte.image = game.assets["icon0.gif"];
+  morte.frame = 11;
+
+  morte.addEventListener("enterframe", function (e) {
+    if (this.intersect(bear)) {
+      game.rootScene.removeChild(this);
+      game.score -= 10;
+    } else {
+      this.y += 5;
+    }
+  });
+  game.rootScene.addChild(morte);
+}
+
+function addBomba() {
   var bomba = new Sprite(16, 16);
   bomba.x = rand(320);
   bomba.y = 0;
@@ -87,11 +136,15 @@ function addBanana() {
   bomba.addEventListener("enterframe", function (e) {
     if (this.intersect(bear)) {
       game.rootScene.removeChild(this);
-      game.score--; 
-      this.y += 3;
+      game.score--; // Reduz a pontuação quando a bomba toca o personagem
+    } else {
+      this.y += 4;
     }
   });
+  game.rootScene.addChild(bomba);
+}
 
+function addDinamite() {
   var dinamite = new Sprite(16, 16);
   dinamite.x = rand(320);
   dinamite.y = 0;
@@ -107,46 +160,9 @@ function addBanana() {
     }
   });
 
-  var estrela = new Sprite(16, 16);
-  estrela.x = rand(320);
-  estrela.y = 0;
-  estrela.image = game.assets["icon0.gif"];
-  estrela.frame = 30;
-
-  estrela.addEventListener("enterframe", function (e) {
-    if (this.intersect(bear)) {
-      game.rootScene.removeChild(this);
-      game.score += 3;
-    } else {
-      this.y += 5;
-    }
-  });
-
-  var morte = new Sprite(16, 16);
-  morte.x = rand(320);
-  morte.y = 0;
-  morte.image = game.assets["icon0.gif"];
-  morte.frame = 11;
-
-  morte.addEventListener("enterframe", function (e) {
-    if (this.intersect(bear)) {
-      game.rootScene.removeChild(this);
-      game.score -= 10;
-    } else {
-      this.y += 5;
-    }
-  });
-
-  game.rootScene.addChild(banana);
-  game.rootScene.addChild(bomba);
-  if (game.score > 10 && game.score < 25) {
-    game.rootScene.addChild(dinamite);
-  }
-  if (game.score > 25) {
-    game.rootScene.addChild(morte);
-    game.rootScene.addChild(estrela);
-  }
+  game.rootScene.addChild(dinamite);
 }
+
 function rand(num) {
   return Math.floor(Math.random() * num);
 }
@@ -155,14 +171,14 @@ function victory() {
   
   var winImage = new enchant.Sprite();
 
- 
+  
   winImage.image = game.assets["win.png"];
 
  
   winImage.x = 22;
   winImage.y = 15;
-  winImage.width = 250; // Ajuste para a largura
-  winImage.height = 250; // Ajuste para a altura 
+  winImage.width = 250; 
+  winImage.height = 250; 
 
   
   var victoryScene = new enchant.nineleap.SplashScene();
